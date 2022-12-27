22c43c99408d5f32b8c8d4fd3f7af4dd6c97f992
Ayannasamal <96683706+Ayannasamal@users.noreply.github.com>
2022-12-27 20:12:31 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,17 +1,17 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCHAizjpnzgNrZ2rQZtKOtpRS0h0RS6gaQ",
-    authDomain: "covid19bot-gxul.firebaseapp.com",
-    databaseURL: "https://covid19bot-gxul-default-rtdb.firebaseio.com",
-    projectId: "covid19bot-gxul",
-    storageBucket: "covid19bot-gxul.appspot.com",
-    messagingSenderId: "757999173122",
-    appId: "1:757999173122:web:2906e6b4fe774747e2f11b",
-    measurementId: "G-1SYCXTMC3H"
+    apiKey: "AIzaSyCKfrrk7JLtcQ9rWARUqqHj0G9J3ZI0PKI",
+    authDomain: "let-s-chat-web-app-a6b8c.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-a6b8c-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-a6b8c",
+    storageBucket: "let-s-chat-web-app-a6b8c.appspot.com",
+    messagingSenderId: "822058065692",
+    appId: "1:822058065692:web:d5a6a9ebe14c7f4f6d64d9",
+    measurementId: "G-3GBBS5G8YZ"
   };
 
   firebase.initializeApp(firebaseConfig);
   user_save1=localStorage.getItem("user_name");
-  document.getElementById("user_name").innerHTML="welcome"+user_save1+"!";
+  document.getElementById("user_name").innerHTML="Welcome "+user_save1+"!";
   function add_room(){
     room_name=document.getElementById("pls_type_sir").value;
     firebase.database().ref("/").child(room_name).update({ purpose : "adding room name" });
@@ -29,7 +29,7 @@ var firebaseConfig = {
       localStorage.setItem("room_name", name);
       window.location = "kwitter_page.html";
      }
-     function get_out(){
+     function logOut(){
            localStorage.removeItem("user_name");
            localStorage.removeItem("room_name");
            window.location="index.html";
